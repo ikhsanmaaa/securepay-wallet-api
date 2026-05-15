d9c1032a1bac741cd68fcfdd364496b2ac83e759
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-05-15 10:06:24 +0700

    init

---
+++ src/main/java/com/ikhsan/securepaywallet/SecurepayWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.ikhsan.securepaywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepayWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepayWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikhsan/securepaywallet/SecurepayWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikhsan.securepaywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepayWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
