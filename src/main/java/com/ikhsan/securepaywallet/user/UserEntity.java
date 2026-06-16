f216f313e05ae4d1bd7bd6940cf519486b004514
ikhsanmaulana <ikhsanmaaa@gmail.com>
2026-06-16 23:53:53 +0700

    add repo and entity user

---
+++ src/main/java/com/ikhsan/securepaywallet/user/UserEntity.java
@@ -0,0 +1,39 @@
+package com.ikhsan.securepaywallet.user;
+
+import java.util.UUID;
+
+import com.ikhsan.securepaywallet.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    @Email
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+}
